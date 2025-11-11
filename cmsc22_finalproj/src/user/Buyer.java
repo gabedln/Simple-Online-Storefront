de554871c4a2984cf43ad45a35a26ffaec1af995
gabedln <mddeleon7@up.edu.ph>
2025-11-11 16:54:10 +0800

    Updated "Buyer" class information and attributes.

---
+++ cmsc22_finalproj/src/user/Buyer.java
@@ -0,0 +1,40 @@
+package user;
+
+import product.*;
+import java.util.ArrayList;
+
+public class Buyer extends User{
+	ArrayList<Product> shoppingCart;
+	ArrayList<Product> wishlist;
+	String transactionLog;
+
+	public Buyer(String username, String password, String displayName, double balance, String location) {
+		super(username, password, displayName, balance, location);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void viewDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeDisplayName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeLocation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
