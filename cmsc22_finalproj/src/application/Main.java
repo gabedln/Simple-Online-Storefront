77c795ecbb9eae6bb05f9a14f43f15ea082407c0
gabedln <mddeleon7@up.edu.ph>
2025-11-13 01:16:11 +0800

    Updated CSS formatting as well as initial menu screen.

--- cmsc22_finalproj/src/application/Main.java
+++ cmsc22_finalproj/src/application/Main.java
@@ -1,10 +1,19 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 public class Main extends Application {
 	@Override
@@ -15,6 +24,53 @@ public class Main extends Application {
 			Scene scene = new Scene(root,1024,576);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.setTitle("Girl, Boy, Bakla, Tomboy Store");
+			
+			TextField emailField = new TextField();
+			emailField.setPromptText("email");
+			emailField.getStyleClass().add("text-field");
+			emailField.setMinWidth(255);
+			
+			PasswordField pwField = new PasswordField();
+			pwField.setPromptText("password");
+			emailField.getStyleClass().add("text-field");
+			// GridPane sets password field and email field to be the same, appearance wise
+			
+			Button login = new Button("login");
+			login.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				public void handle(MouseEvent arg0) {
+					System.out.println("Login Button Clicked!");
+				}
+			});
+			
+			login.setMinWidth(255);
+			login.getStyleClass().add("login-button");		
+			
+			Label signupLabel = new Label("don't have an account?");
+			signupLabel.getStyleClass().add("signup-label");
+			
+			Hyperlink signupLink = new Hyperlink ("Sign up here.");
+			signupLink.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				public void handle(MouseEvent arg0) {
+					System.out.println("Sign-up Button Clicked!");
+				}
+			});
+			signupLink.getStyleClass().add("signup-link");
+			HBox signupPrompt = new HBox (4, signupLabel, signupLink);
+			
+			
+			
+			GridPane grid = new GridPane();
+			grid.setHgap(10);
+			grid.setVgap(10);
+			grid.setStyle("-fx-padding: 200 20 20 20;"); // -fx-padding: top right bottom left
+			grid.setAlignment(Pos.CENTER);
+			
+			grid.addRow(0, emailField);
+			grid.addRow(1, pwField);
+			grid.addRow(2, login);
+			grid.addRow(3, signupPrompt);
+			
+			root.setCenter(grid);
 			stage.setScene(scene);
 			stage.show();
 		} catch(Exception e) {
--- cmsc22_finalproj/src/module-info.java
+++ cmsc22_finalproj/src/module-info.java
@@ -1,5 +1,6 @@
 module cmsc22_finalproj {
 	requires javafx.controls;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
