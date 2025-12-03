bb587fa4b0fa7e5cf62716b2f66cc24a4a2e7f08
Avigail Ann A. Monsalud <aamonsalud@up.edu.ph>
2025-12-03 08:44:24 +0800

    Update User.java

--- cmsc22_finalproj/src/user/User.java
+++ cmsc22_finalproj/src/user/User.java
@@ -11,41 +11,34 @@ package user;
 import java.io.Serializable;
 
 public abstract class User implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String displayName;
-	private String username;
-	private String password;
-	private float balance;
-	private String location;
-	
-	public User(String displayName, String username, String password, float balance, String location) {
-		this.displayName = displayName;
-		this.username = username;
-		this.password = password;
-		this.balance = balance;
-		this.location = location;
-	}
-	
-	public abstract void displayDashboard();
-	
-	
-	public void changeDisplayName(String name) {
-		this.name = name;
-	}
-	
-	public void changeLocation(String location) {
-		this.location = location;
-	}
-	
-	
-	public String getDisplayName() { return this.displayName; }
-	public String getName() { return this.username; }
-	public String getPassword() { return this.password; }
-	public String getUsername() { return this.username; }
-	public String getLocation() { return this.location; }
-	public float getBalance() { return this.balance; }
-	public void setBalance(float balance) { this.balance = balance; }
+    private static final long serialVersionUID = 1L;
 
+    private String displayName;
+    private String username;
+    private String password;
+    private String location;
+    private float balance; 
+
+    public User(String displayName, String username, String password, float balance, String location) {
+        this.displayName = displayName;
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+        this.location = location;
+    }
+
+    // Abstract method for dashboards
+    public abstract void displayDashboard();
+
+    // Mutators
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+    public void setLocation(String location) { this.location = location; }
+    public void setBalance(float balance) { this.balance = balance; }
+
+    // Accessors
+    public String getDisplayName() { return displayName; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getLocation() { return location; }
+    public float getBalance() { return balance; }
 }
